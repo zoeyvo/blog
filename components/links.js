807f21f104891444afd0910a8359bd42972ae267
zoeyvo <zoeyvo256@gmail.com>
2025-04-08 04:39:24 -0700

    link change

--- components/links.js
+++ components/links.js
@@ -1,5 +1,3 @@
 export const asideLinks = [
-    { href: '/post1', label: 'Post 1' },
-    { href: '/post2', label: 'Post 2' },
-    { href: '/post3', label: 'Post 3' },
+    { href: 'https://mydoctor.kaiserpermanente.org/ncal/structured-content/hormone-therapy-with-estrogen-and-androgen-blockers-1123088', label: 'MTF HRT INFO' },
 ];
