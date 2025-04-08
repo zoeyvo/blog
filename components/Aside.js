0c4c623e41575b677f22fba3800b912b3d1c0d05
zoeyvo <zoeyvo256@gmail.com>
2025-04-08 12:35:00 -0700

    v6

--- components/Aside.js
+++ components/Aside.js
@@ -7,7 +7,7 @@ export default function Aside() {
 
     return `
         <aside>
-            <h2>resources</h2>
+            <h2>LINKS</h2>
             <ul>
                 ${asideLinks.map(link => `<li><a href="${link.href}" target="_blank" rel="noopener noreferrer">${link.label}</a></li>`).join('')}
             </ul>
--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,21 @@
 export default function Nav() {
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    
     return `
         <nav aria-label="Main Navigation">
             <ul>
-                <li><a href="index.html"><img src="assets/world.png" alt="home" class="icon" height="25"></a></li>
-                <li><a href="projects.html"><img src="assets/folder-type.png" alt="projects" class="icon" height="33"></a></li>
-                <li><a href="posts.html"><img src="assets/warning.png" alt="posts" class="icon" height="29"></a></li>
-                <li><a href="about.html"><img src="assets/person.png" alt="about" class="icon" height="25"></a></li>
+                <li><a href="index.html" class="${currentPage === 'index.html' ? 'active' : ''}">
+                    <img src="assets/world.png" alt="home" class="icon" height="25">
+                </a></li>
+                <li><a href="projects.html" class="${currentPage === 'projects.html' ? 'active' : ''}">
+                    <img src="assets/folder-type.png" alt="projects" class="icon" height="33">
+                </a></li>
+                <li><a href="posts.html" class="${currentPage === 'posts.html' ? 'active' : ''}">
+                    <img src="assets/warning.png" alt="posts" class="icon" height="29">
+                </a></li>
+                <li><a href="about.html" class="${currentPage === 'about.html' ? 'active' : ''}">
+                    <img src="assets/person.png" alt="about" class="icon" height="25">
+                </a></li>
             </ul>
         </nav>
     `;
